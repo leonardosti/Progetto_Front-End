1cce77b8ada6ff98b0b9c6a924b2f906a53efbb0
MatteoCominato <matteo.cominato@iisviolamarchesini.edu.it>
2024-05-23 18:05:04 +0200

    Aggiunto Adidas.js

---
+++ SitoWeb/scriptAdidas.js
@@ -0,0 +1,49 @@
+const button = document.body.querySelectorAll('button');
+button[1].addEventListener('click', function (){
+    
+    let p = document.createElement('p');
+    p.textContent = "Le celebrazioni del 30esimo anniversario di BAPE continuano anche nel 2024 con l’arrivo di una nuova collaborazione tra il marchio fondato dal designer Nigo e adidas Originals. La scarpa appena presentata vanta una tomaia realizzata in pelle premium e abbellita dall’iconica stampa camouflage di BAPE nelle tonalità del verde e del marrone.";
+    var div = document.getElementById('1');
+    div.appendChild(p);
+    button[1].style.display = 'none';
+})
+button[2].addEventListener('click', function (){
+    
+    let p = document.createElement('p');
+    p.textContent = "Se dovessimo ripercorrere il ritorno in cattedra di adidas in seguito all’abbandono di Yeezy non potremmo non citare silhouette come quelle delle adidas Samba, delle Gazelle e infine quella delle adidas Campus, modello che dopo aver spopolato online è diventato pressoché introvabile. La silhouette dai volumi chunky, contraddistinta da una tomaia in suede colorato e dalla presenza di una fat tongue e dei fat lace bianchi, è però finalmente tornata disponibile.";
+    var div = document.getElementById('2');
+    div.appendChild(p);
+    button[2].style.display = 'none';
+})
+button[3].addEventListener('click', function (){
+    
+    let p = document.createElement('p');
+    p.textContent = "La collaborazione tra Wales Bonner e adidas è pronta a tornare e a espandersi. Per l’occasione il duo ha messo mano alla versione originaria delle adidas Samba, una delle più iconiche scarpe Adidas.";
+    var div = document.getElementById('3');
+    div.appendChild(p);
+    button[3].style.display = 'none';
+})
+button[4].addEventListener('click', function (){
+    
+    let p = document.createElement('p');
+    p.textContent = "Le Yeezy Foam Runner rappresentino una vera e propria rivoluzione nel campo del footwear. La calzatura, che ha tutte le carte in regola per stravolgere il mondo del footwear in termini di comodità, tempi di produzione e costi, dovrà inoltre estraniarsi dalla hype culture che aleggia intorno a moltissime sneakers.";
+    var div = document.getElementById('4');
+    div.appendChild(p);
+    button[4].style.display = 'none';
+})
+button[5].addEventListener('click', function (){
+    
+    let p = document.createElement('p');
+    p.textContent = "La 350 V2 crea un gioco di colore audace con la sua tomaia composta da Primeknit in tre tonalità di filato da grande impatto. Una striscia di “Solar Red” fa risaltare “SPLY-350” e i lati in TPU creano un suggestivo effetto fornendo in contemporanea una trazione superba.";
+    var div = document.getElementById('5');
+    div.appendChild(p);
+    button[5].style.display = 'none';
+})
+button[6].addEventListener('click', function (){
+    
+    let p = document.createElement('p');
+    p.textContent = "Come ogni scarpa firmata adidas Originals, anche la L.A. Trainer vanta una storia legata a eventi sportivi e innovazioni tecnologiche che hanno rivoluzionato il mondo delle calzature da ginnastica. Siamo nel 1984 e i Giochi Olimpici ritornano negli Stati Uniti, più precisamente a Los Angeles, città da cui prendono il nome queste sneakers.";
+    var div = document.getElementById('6');
+    div.appendChild(p);
+    button[6].style.display = 'none';
+})
